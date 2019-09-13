055b541774deab7b2d0125d7841c9a5bc7f7484d
Hassan Gado <hgado11@gmail.com>
2019-09-13 15:09:59 +0300

    Initial Commit

--- Movies/ViewControllers/MainViewController.swift
+++ Movies/ViewControllers/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  Movies
 //
 //  Created by Hassan Gado on 9/13/19.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
